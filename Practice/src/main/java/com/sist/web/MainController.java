a724398d7f4ee2acd8be5f7a92f27dce9d1de326
admin <jiwon3502@naver.com>
2022-09-15 17:18:04 +0900

    11

---
+++ Practice/src/main/java/com/sist/web/MainController.java
@@ -0,0 +1,18 @@
+package com.sist.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("main/main.do")
+	public String main_main() {
+		
+		return "main";
+	}
+}
